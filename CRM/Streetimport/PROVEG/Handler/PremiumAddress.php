3d62d132aac14307d6ef9b848fb0df364bc7e460
B. Endres <endres@systopia.de>
2018-10-15 10:03:53 +0200

    implementing Premium [WIP]

--- CRM/Streetimport/PROVEG/Handler/PremiumAddress.php
+++ CRM/Streetimport/PROVEG/Handler/PremiumAddress.php
@@ -17,7 +17,7 @@ class CRM_Streetimport_PROVEG_Handler_PremiumAddress extends CRM_Streetimport_PR
   /**
    * Check if the given handler implementation can process the record
    *
-   * @param $record  an array of key=>value pairs
+   * @param $record array an array of key=>value pairs
    * @return true or false
    */
   public function canProcessRecord($record, $sourceURI) {
@@ -30,7 +30,7 @@ class CRM_Streetimport_PROVEG_Handler_PremiumAddress extends CRM_Streetimport_PR
   /**
    * process the given record
    *
-   * @param $record  an array of key=>value pairs
+   * @param $record array an array of key=>value pairs
    * @return true
    * @throws exception if failed
    */
@@ -38,23 +38,73 @@ class CRM_Streetimport_PROVEG_Handler_PremiumAddress extends CRM_Streetimport_PR
     $config = CRM_Streetimport_Config::singleton();
     CRM_Core_Error::debug_log_message("Looking at record [{$record['UebgID']}]", $record);
 
-    // STEP 1: extract data
-    $old_data  = $this->extractAddress($record, 'E_');
-    $new_data  = $this->extractAddress($record, 'NSA_');
+    // find contact
+    $contact = $this->identifyContact($record);
 
-    // STEP 2: find contact and address
-    $contacts  = $this->findContacts($old_data);
-    $addresses = $this->findAddresses($contacts);
-    $address   = $this->selectAddressMatch($addresses, $new_data);
+    if (!$contact) {
+      // if contact not found: create activity
+      $this->createPremiumActivity($contact, $record, 'Contact not found.', 'Scheduled');
+      $this->getLogger()->logImport($record, false, $config->translate('Premium Address'));
 
-    // STEP 3: update address
+    } else {
+      // get address data
 
+      // contact found:
+      switch ($record['AdrMerk']) {
+        case 10:
+          # "Empfänger / Firma unter der angegebenen Anschrift nicht zu ermitteln"
+          # Kd_: ja, E_: nein, _NSA: nein
+          $this->createPremiumActivity($contact, $record, 'Contact not known', 'Scheduled');
+          $this->getLogger()->logImport($record, true, $config->translate('Premium Address'));
+          break;
 
-    // STEP 4: create activity
+        case 12:
+          # Empfänger soll verstorben sein
+          # Kd_: ja, E_: teilw., _NSA: nein
+          // TODO: mark deceased?
+          $this->createPremiumActivity($contact, $record, 'Contact deceased', 'Scheduled');
+          $this->getLogger()->logImport($record, true, $config->translate('Premium Address'));
+          break;
 
+        case 14:
+          # Empfänger / Firma unter der angegebenen Anschrift nicht zu ermitteln, Qualifiziert durch Datenbank
+          # Kd_: ja, E_: ja, _NSA: nein
+          $this->deleteAddresses($contact, $record);
+          $this->createPremiumActivity($contact, $record, 'Couldn\'t reach', 'Completed');
+          $this->getLogger()->logImport($record, true, $config->translate('Premium Address'));
+          break;
 
+        case 20:
+          # Empfänger verzogen
+          #  Kd_: ja, E_: ja, _NSA: ja
+          $this->addAddress($contact, $record);
+          $this->deleteAddresses($contact, $record);
+          $this->createPremiumActivity($contact, $record, 'Moved', 'Completed');
+          $this->getLogger()->logImport($record, true, $config->translate('Premium Address'));
+          break;
 
-    $this->logger->logImport($record, true, $config->translate('Premium Address'));
+        case 21:
+          # Empfänger verzogen, Einwilligung zur Weitergabe der neuen Anschrift liegt nicht vor
+          #  Kd_: ja, E_: ja, _NSA: nein
+          $this->deleteAddresses($contact, $record);
+          $this->createPremiumActivity($contact, $record, 'Moved (address unknown)', 'Completed');
+          $this->getLogger()->logImport($record, true, $config->translate('Premium Address'));
+          break;
+
+        case 31:
+          # Mängel in Adresse (PLZ, Ort, Straße, Hausnummer, Postfach)
+          # Kd_: ja, E_: teilw., _NSA: nein
+          $this->deleteAddresses($contact, $record);
+          $this->createPremiumActivity($contact, $record, 'Moved (address unknown)', 'Completed');
+          $this->getLogger()->logImport($record, true, $config->translate('Premium Address'));
+          break;
+
+        default:
+          $this->createPremiumActivity($contact, $record, "Unknown reply [{$record['AdrMerk']}]", 'Scheduled');
+          $this->getLogger()->logImport($record, false, $config->translate('Premium Address'));
+          break;
+      }
+    }
   }
 
   /**
@@ -65,27 +115,56 @@ class CRM_Streetimport_PROVEG_Handler_PremiumAddress extends CRM_Streetimport_PR
    */
   protected function extractAddress($record, $prefix = '') {
     $data = [
-        'first_name'  => CRM_Utils_Array::value("{$prefix}Na1",  $record, ''),
-        'last_name'   => CRM_Utils_Array::value("{$prefix}Na2",  $record, ''),
+        'name_1'      => CRM_Utils_Array::value("{$prefix}Na1",  $record, ''),
+        'name_2'      => CRM_Utils_Array::value("{$prefix}Na2",  $record, ''),
         'name_3'      => CRM_Utils_Array::value("{$prefix}Na3",  $record, ''),
         'name_4'      => CRM_Utils_Array::value("{$prefix}Na4",  $record, ''),
         'street'      => CRM_Utils_Array::value("{$prefix}Str",  $record, ''),
         'number'      => CRM_Utils_Array::value("{$prefix}HNr",  $record, ''),
         'postal_code' => CRM_Utils_Array::value("{$prefix}PLZ",  $record, ''),
         'city'        => CRM_Utils_Array::value("{$prefix}Ort",  $record, ''),
-        'country'     => CRM_Utils_Array::value("{$prefix}Land", $record, ''),
     ];
 
     // compile address
     $data['street_address'] = trim("{$data['street']} {$data['number']}");
 
-    // default country is DE
-    if (empty($data['country'])) {
-      $data['country'] = 'DE';
-    }
 
     return $data;
   }
+
+
+  /**
+   * Tries to identify the contact and all releated addresses
+   *
+   * @param array $record
+   * @return array with contact data, with an entry 'addresses' with an array of all matching addresses
+   */
+  protected function identifyContact($record) {
+    // TODO
+  }
+
+
+  protected function createPremiumActivity($contact, $record, $subject, $status) {
+    // TODO
+  }
+
+  protected function deleteAddresses($contact, $record) {
+    // TODO
+  }
+
+  protected function addAddress($contact, $record) {
+    $entry_new = $this->extractAddress($record, 'NSA_');
+    // TODO
+  }
+
+
+
+
+
+
+
+
+
 
   /**
    * Find all contacts matching first and last name
@@ -94,7 +173,12 @@ class CRM_Streetimport_PROVEG_Handler_PremiumAddress extends CRM_Streetimport_PR
    * @throws CiviCRM_API3_Exception
    * @return array contacts
    */
-  protected function findContacts($contact_data) {
+  protected function findContacts($contact_data, $record) {
+    if (empty($contact_data['first_name']) || empty($contact_data['last_name'])) {
+      $this->getLogger()->logMessage("No name given", $record);
+      return [];
+    }
+
     $contacts = civicrm_api3('Contact', 'get', [
         'first_name'   => $contact_data['first_name'],
         'last_name'    => $contact_data['last_name'],
@@ -113,7 +197,7 @@ class CRM_Streetimport_PROVEG_Handler_PremiumAddress extends CRM_Streetimport_PR
    * @throws CiviCRM_API3_Exception
    * @return array addresses
    */
-  protected function findAddresses($contacts) {
+  protected function findAddresses($contacts, $record) {
     if (empty($contacts)) {
       // no contacts found
       return [];
@@ -137,7 +221,7 @@ class CRM_Streetimport_PROVEG_Handler_PremiumAddress extends CRM_Streetimport_PR
    *
    * @return array|null an address if one over the threshold is found.
    */
-  protected function selectAddressMatch($addresses, $sample) {
+  protected function selectAddressMatch($addresses, $sample, $record) {
     $fields = ['street_address', 'postal_code', 'city'];
     $highscore  = 0.0;
     $highscorer = NULL;
